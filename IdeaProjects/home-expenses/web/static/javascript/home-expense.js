c5cd2da819f8dc02e6b60a241438d2c57d8e900d
bharath2k2in <bharath2k2in@gmail.com>
2014-04-10 23:53:03 +0530

    added input validation
    yet to do : clearing input

--- IdeaProjects/home-expenses/web/static/javascript/home-expense.js
+++ IdeaProjects/home-expenses/web/static/javascript/home-expense.js
@@ -4,30 +4,50 @@ $(document).ready(function () {
 
 function submitIncomeExpenseDetails() {
 
-    var amount = parseFloat($("#amount").val());
-    var description = $("#desc").val();
+    var amountDiv = $("#amount");
+    var descriptionDiv = $("#desc");
+    var expenseCategoryDiv = $("#expenseCategory");
+    var incomeCategoryDiv = $("#incomeCategory");
+
+    var amount = parseFloat(amountDiv.val());
+    var description = descriptionDiv.val();
     var amountType = $('input[name$="amountType"]:checked').val();
 
     var category;
-    if ($("#expenseCategory").css("display") === "inline-block") {
-        category = $("#expenseCategory option:selected").text();
-    } else if ($("#incomeCategory").css("display") === "inline-block") {
-        category = $("#incomeCategory option:selected").text();
+
+    if (expenseCategoryDiv.is(':visible')) {
+        category = expenseCategoryDiv.find('option:selected').text();
+    } else if (incomeCategoryDiv.is(':visible')) {
+        category = incomeCategoryDiv.find('option:selected').text();
     }
 
-    $.ajax({
-        url: "http://localhost:8080/home-expenses/expense/add",
-        type: "POST",
-        data: JSON.stringify({amount: amount, description: description, category: category, amountType: amountType}),
-        contentType: "application/json",
-        dataType: "script",
-        success: function (data) {
-        },
-        error: function (e) {
-            console.log(JSON.stringify(e));
-        }
-    });
+    if (isNaN(amount) || category === "Category" || description.trim() === "") {
+        alert("Wrong input");
+    } else {
+        $.ajax({
+            url: "http://localhost:8080/home-expenses/expense/add",
+            type: "POST",
+            data: JSON.stringify({amount: amount, description: description, category: category, amountType: amountType}),
+            contentType: "application/json",
+            dataType: "script",
+            success: function (data) {
+                clearInput();
+            },
+            error: function (e) {
+                console.log(JSON.stringify(e));
+            }
+        });
 
+        var clearInput = function() {
+            amountDiv.val("");
+            descriptionDiv.val("");
+            if (expenseCategoryDiv.css("display") === "inline-block") {
+                expenseCategoryDiv.find('option:first').attr('selected', 'selected');
+            } else if (incomeCategoryDiv.css("display") === "inline-block") {
+                incomeCategoryDiv.find('option:first').attr('selected', 'selected');
+            }
+        }
+    }
 }
 
 function showIncomeCategories() {
